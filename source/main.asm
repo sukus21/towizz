72b3d72d87f602105ca8c1d6f02598fdd4ad0f0d
sukus <52664542+sukus21@users.noreply.github.com>
2023-07-02 12:44:37 +0200

    LYC interrupt setup
    New macros and interrupt vectors

--- source/main.asm
+++ source/main.asm
@@ -2,6 +2,10 @@ INCLUDE "hardware.inc"
 
 SECTION "ENTRY POINT", ROM0[$0100]
 
+; Entrypoint of the program.
+; Do not call manually.
+; Lives in ROM0.
+entrypoint:
     ;Disable interupts and jump
     di
     jp setup
@@ -10,6 +14,17 @@ SECTION "ENTRY POINT", ROM0[$0100]
     ds $4C, $00
 ;
 
+
+
+SECTION "VBLANK INTERRUPT", ROM0[$0040]
+
+; Vblank interrupt vector.
+; Does nothing, as this is not how I detect Vblank.
+; Does NOT set IME when returning.
+; Lives in ROM0.
+v_vblank::
+    ret
+;
 
 
 SECTION "MAIN", ROM0[$0150]
